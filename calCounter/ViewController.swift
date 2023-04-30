b5da01b803080b360377c42dd734857beab5cdf9
Alex <alexnosatti@gmail.com>
2023-04-30 13:38:10 +1000

    Initial Commit

---
+++ calCounter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  calCounter
+//
+//  Created by Alex Nosatti on 30/4/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
