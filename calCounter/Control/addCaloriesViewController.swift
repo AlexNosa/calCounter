95c06252a880fbeed5f33f322583c66c368ffab2
kingsleybyrne <kingsley.r.byrne@gmail.com>
2023-05-08 21:29:52 +1000

    Button Pictures added
    Pictures added for buttons - need to implement

--- calCounter/Control/addCaloriesViewController.swift
+++ calCounter/Control/addCaloriesViewController.swift
@@ -13,6 +13,15 @@ class addCaloriesViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+//get the calories using this call
+//    //let query = "3lb carrots and a chicken sandwich".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+//    let url = URL(string: "https://api.calorieninjas.com/v1/nutrition?query="+query!)!
+//    var request = URLRequest(url: url)
+//    request.setValue("YOUR_API_KEY", forHTTPHeaderField: "eHLXvaQ8P8N40UMSrZh85A==78XyI3BrPnl7SE6n")
+//    let task = URLSession.shared.dataTask(with: request) {(data, response, error) in
+//        guard let data = data else { return }//get calories using getter
+//        print(String(data: data, encoding: .utf8)!)
+//    }
+//    task.resume()
 
 }
