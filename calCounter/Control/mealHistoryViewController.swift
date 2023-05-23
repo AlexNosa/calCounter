f4c7036ef6e74f58c1d505f7e631f8f0fe127726
kingsleybyrne <kingsley.r.byrne@gmail.com>
2023-05-23 14:01:12 +1000

    tableview fixed

--- calCounter/Control/mealHistoryViewController.swift
+++ calCounter/Control/mealHistoryViewController.swift
@@ -3,46 +3,34 @@
 //
 //  Created by Alex Nosatti on 30/4/2023.
 //
+ import Foundation
+ import UIKit
 
-import Foundation
-import UIKit
+ struct meal{
+     var foodName:String
+     var calorieCount:Int
+ }
+ class mealHistoryViewController : UIViewController {
 
-struct meal{
-    var foodName:String
-    var calorieCount:Int
-}
-class mealHistoryViewController : UIViewController, UITableViewDelegate {
-    
-    @IBOutlet weak var mealHistoryTableView: UITableView!
-    var mealHistory = [meal]()
-    // You can also implement other delegate methods based on your needs.
-    override func viewDidLoad() {
-        super.viewDidLoad()
+     @IBOutlet weak var mealHistoryTableView: UITableView!
+     var mealHistory = [meal]()
+     // You can also implement other delegate methods based on your needs.
+ }
 
-        mealHistoryTableView.delegate = self
-        mealHistoryTableView.dataSource = self
+ extension  mealHistoryViewController:UITableViewDelegate {
 
-        mealHistoryTableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-    }
+ }
 
-    
-}
+ extension mealHistoryViewController:UITableViewDataSource {
+     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+         return mealHistory.count
+     }
 
-extension mealHistoryViewController:UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return mealHistory.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        let food = mealHistory[indexPath.row]
-        cell.textLabel?.text = food.foodName
-        cell.detailTextLabel?.text = String(food.calorieCount)
-        return cell
-    }
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "Food: \t Calories:"
-    }
-
-
-}
+     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+         let food = mealHistory[indexPath.row]
+         cell.textLabel?.text = food.foodName
+         cell.detailTextLabel?.text = String(food.calorieCount)
+         return cell
+     }
+ }
